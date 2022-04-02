89386e57634e6aeda03fe97ce83b1d51582711a2
Asim Ali Khan <63301189+D0T7@users.noreply.github.com>
2022-04-02 14:58:32 +0530

    Update instagramRedirect.js
    added the script

--- instagramRedirect.js
+++ instagramRedirect.js
@@ -1,1 +1,42 @@
 
+// ==UserScript==
+// @name Instagram Redirect
+// @namespace https://github.com/D0T7/Instagram-Redirect/
+// @version 0.1
+// @description	Redirect Instagram
+// @run-at document-start
+// @match *://instagram.com/*
+// @match *://*.instagram.com/*
+
+// ==/UserScript==
+
+let redirectInstagram = () => {
+
+  let url = "dumpor.com"; // change to some other ig viewer if this isn't working.
+  let key = "/v"; // when you open a profile with the viewer it will show some other value  , eg dumpor/v/profile, set it to that,
+  //and if it is not there , set it to "".
+
+  if (window.location.pathname.indexOf("/p/") == 0) {
+    window.stop();
+    location.hostname = url;
+  } else if (window.location.pathname == "/") {
+    window.stop();
+    location.hostname = url;
+  } else if (window.location.pathname === "/accounts/login/") {
+    window.stop();
+    let oldQuery = window.location.search;
+    let newQuery = oldQuery.replace("?next=/", "/");
+    let newURL =
+      window.location.protocol + "//" + url + key + newQuery +
+      window.location.hash;
+    window.location.replace(newURL);
+  } else {
+    window.stop();
+    let oldUrlPath = window.location.pathname;
+    let newURL =
+      window.location.protocol + "//" + url + key + oldUrlPath + window.location.search + window.location.hash;
+    window.location.replace(newURL);
+  }
+};
+
+redirectInstagram();
